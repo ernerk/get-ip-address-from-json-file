b09a40d4b8085e91e36f9c48973b2767fa8a2b0c
ernerk <47643387+ernerk@users.noreply.github.com>
2023-07-13 21:14:33 +0300

    import json.py

--- import json.py
+++ import json.py
@@ -3,28 +3,28 @@ import re
 import pandas as pd
 
 def find_ips(input_string):
-    # Basit bir IP adresi regex deseni
+    # Simple IP address regex pattern
     ip_pattern = r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
     ips = re.findall(ip_pattern, input_string)
     return ips
 
-# Tüm IP adreslerini bir liste içinde toplamak için
+# Collect all IP addresses in a list
 all_ips = []
 
-# json dosyasını aç ve her satırı ayrı bir json nesnesi olarak oku
+# Open the json file and read each line as a separate json object
 with open(r"C:\Users\erene\Desktop\shodanip.json", 'r') as f:
     for line in f:
         data = json.loads(line)
         for key, value in data.items():
-            if isinstance(value, str):  # değer bir string ise
+            if isinstance(value, str):  # if the value is a string
                 all_ips.extend(find_ips(value))
-            elif isinstance(value, list):  # değer bir liste ise
+            elif isinstance(value, list):  # if the value is a list
                 for item in value:
                     if isinstance(item, str):
                         all_ips.extend(find_ips(item))
 
-# IP adreslerini bir pandas DataFrame'ine dönüştür
-df = pd.DataFrame(all_ips, columns=['IP Adresleri'])
+# Convert the IP addresses into a pandas DataFrame
+df = pd.DataFrame(all_ips, columns=['IP Addresses'])
 
-# DataFrame'i bir Excel dosyasına yaz
-df.to_excel(r"C:\Users\erene\Desktop\ip_adresleri.xlsx", index=False)
+# Write the DataFrame to an Excel file
+df.to_excel(r"C:\Users\erene\Desktop\ip_addresses.xlsx", index=False)
